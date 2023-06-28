1bdb6a20e9778f70f9ed0971b90ce5aaa688376f
Odiseoreo <42281486+Kuuhaku07@users.noreply.github.com>
2023-06-27 23:53:44 -0400

    Arreglado el movimiento, Por Arreglar friccion y salto

--- Scenes/Faith.gd
+++ Scenes/Faith.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 @export var Max_Speed = 300
-@export var Aceleracion = Vector2(1500, 0)
-@export var Friccion = 1200
+@export var Aceleracion = Vector2(1500, 1500)
+@export var Friccion = 1000
 @onready var axis = Vector2.ZERO
 @export var Jump_Speed = 500
 @export var Is_Grounded = true
@@ -16,7 +16,6 @@ extends CharacterBody2D
 func _physics_process(delta):
 	move(delta)
 	var motion = Vector2(axis.x * Max_Speed, axis.y * Jump_Speed)
-	motion = move_and_slide()
 	Is_Grounded = motion
 	if Is_Crouched:
 		set_scale(Vector2(1, 0.5))
@@ -26,10 +25,17 @@ func _physics_process(delta):
 
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("Moverse_Derecha")) - int(Input.is_action_pressed("Moverse_Izquierda"))
-	if Input.is_action_just_pressed("Saltar") and Is_Grounded:
+	
+	
+	if Input.is_action_just_pressed("Saltar") and is_on_floor():
+		axis.y = -1
+	elif Input.is_action_pressed("Saltar") :
 		axis.y = -1
 	elif Input.is_action_just_released("Saltar") and axis.y < 0:
 		axis.y = 0
+	else:
+		axis.y=0.5
+		
 	if Input.is_action_just_pressed("Agacharse") and Is_Grounded:
 		Is_Crouched = true
 	elif Input.is_action_just_released("Agacharse") and Is_Crouched:
@@ -42,10 +48,9 @@ func move(delta):
 	
 	if axis== Vector2.ZERO:
 		apply_friction(Friccion * delta)
-
 	else:
-		var acceleration = Vector2(Aceleracion.x * delta, Aceleracion.y * delta)
-		apply_movement(axis + acceleration)
+		apply_movement(axis * Aceleracion* delta)
+	move_and_slide()
 		
 func apply_friction(amount):
 	if velocity.length()>amount:
