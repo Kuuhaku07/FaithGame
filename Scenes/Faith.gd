7461419053bafabfee64bcd75ffcd652da1bdc29
Odiseoreo <42281486+Kuuhaku07@users.noreply.github.com>
2023-07-01 20:46:43 -0400

    Arreglada gravedad, salto con detalles

--- Scenes/Faith.gd
+++ Scenes/Faith.gd
@@ -1,41 +1,33 @@
 extends CharacterBody2D
 
-@export var Max_Speed = 300
-@export var Aceleracion = Vector2(1500, 1500)
+@export var Max_Speed = 400
+@export var Aceleracion = Vector2(1500, 4000)
 @export var Friccion = 1000
 @onready var axis = Vector2.ZERO
-@export var Jump_Speed = 500
+
 @export var Is_Grounded = true
 @export var Is_Crouched = false
 
+@export var Jump_Max=30000
 
-
-
-
+@export var max_gravity = 1500
+@export var gravity = 1500
 
 func _physics_process(delta):
 	move(delta)
-	var motion = Vector2(axis.x * Max_Speed, axis.y * Jump_Speed)
-	Is_Grounded = motion
+	var motion = Vector2(axis.x * Max_Speed,0)
+	if is_on_floor():
+		Is_Grounded = true
+	else:
+		Is_Grounded=false
 	if Is_Crouched:
 		set_scale(Vector2(1, 0.5))
 	else:
 		set_scale(Vector2(1, 1))
 
-
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("Moverse_Derecha")) - int(Input.is_action_pressed("Moverse_Izquierda"))
 	
-	
-	if Input.is_action_just_pressed("Saltar") and is_on_floor():
-		axis.y = -1
-	elif Input.is_action_pressed("Saltar") :
-		axis.y = -1
-	elif Input.is_action_just_released("Saltar") and axis.y < 0:
-		axis.y = 0
-	else:
-		axis.y=0.5
-		
 	if Input.is_action_just_pressed("Agacharse") and Is_Grounded:
 		Is_Crouched = true
 	elif Input.is_action_just_released("Agacharse") and Is_Crouched:
@@ -45,11 +37,14 @@ func get_input_axis():
 func move(delta):
 	
 	axis=get_input_axis()
-	
+	print(axis)
 	if axis== Vector2.ZERO:
 		apply_friction(Friccion * delta)
-	else:
-		apply_movement(axis * Aceleracion* delta)
+	
+
+	apply_gravity(gravity*delta)
+	apply_movement(axis * Aceleracion* delta)
+	jump(Is_Grounded,delta,Jump_Max)
 	move_and_slide()
 		
 func apply_friction(amount):
@@ -57,7 +52,24 @@ func apply_friction(amount):
 		velocity -=velocity.normalized() * amount
 	else: 
 		velocity= Vector2.ZERO
-		
+
 func apply_movement(accel):
 	velocity += accel
 	velocity = velocity.limit_length(Max_Speed)
+	
+func apply_gravity(accel):
+	if velocity.y<max_gravity:
+		velocity.y += accel
+		
+func jump(grounded,delta,Jump_max):
+	if grounded== true:
+
+		if velocity.y<Jump_max and Input.is_action_just_pressed("Saltar") :
+			velocity.y = Jump_max*-1*delta
+			print (velocity.y)
+
+			
+	
+		
+	
+
